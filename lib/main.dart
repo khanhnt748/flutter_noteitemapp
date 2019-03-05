54523419c37b859ee2104b951c3e79319ccbc2bc
Khanh Nguyen Tan <khanhnt748@Khanhs-MacBook-Pro.local>
2019-03-05 23:53:06 +0700

    - Update UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,91 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(NoteItemApp());
+void main() {
+  runApp(
+    NoteApp()
+  );
+}
 
 class Item {
-  Item(this._name);
+  Item(@required this._name);
   final String _name;
 }
 
+typedef OnTapCallback(Item item);
+typedef OnLongPressCallback(Item item);
 class ItemUI extends StatelessWidget {
+  ItemUI({this.item, this.onTapCallback, this.onLongPressCallback, this.isNeedBuy});
+  final Item item;
+  final bool isNeedBuy;
+  final OnTapCallback onTapCallback;
+  final OnLongPressCallback onLongPressCallback;
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Container(
+      alignment: Alignment.center,
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: ListTile(
+        leading: CircleAvatar(
+          child: Text(item._name[0]),
+          backgroundColor: isNeedBuy ? null : Colors.black45,
+        ),
+        title: Container(
+          child: Text(
+            item._name,
+            style: TextStyle(
+              decoration: isNeedBuy ? null : TextDecoration.lineThrough,
+              fontWeight: isNeedBuy ? FontWeight.bold : FontWeight.normal,
+            ),
+          ),
+        ),
+        onTap: (){ onTapCallback(item); },
+        onLongPress: (){ onLongPressCallback(item); },
+      ),
+    );
   }
 }
 
-class ItemListUI extends StatelessWidget {
+class NoteApp extends StatefulWidget {
+  List<Item> itemList = [Item("Hello"),Item("Test")];
   @override
-  Widget build(BuildContext context) {
-    return null;
-  }
+  NoteAppState createState() => NoteAppState();
 }
 
-class NoteItemApp extends StatefulWidget {
-  @override
-  NoteItemAppState createState() => NoteItemAppState();
-}
-
-class NoteItemAppState extends State<NoteItemApp> {
+class NoteAppState extends State<NoteApp> {
+  var inCartItemList = Set<Item>();
   @override
   Widget build(BuildContext context) {
-    return null;
+    return MaterialApp(
+      title: "Note Item App",
+      home: Scaffold(
+        body: Container(
+          alignment: Alignment.topCenter,
+          child: ListView(
+            children: widget.itemList.map((Item item) {
+              return ItemUI(
+                item: item,
+                isNeedBuy: !inCartItemList.contains(item),
+                onTapCallback: _handleItemOnTap,
+                onLongPressCallback: _handleItemOnLongPress,
+              );
+            }).toList(),
+          ),
+        )
+      )
+    );
   }
 
+  void _handleItemOnTap(item) {
+    setState(() {
+      if (inCartItemList.contains(item)) {
+        inCartItemList.remove(item);
+      } else {
+        inCartItemList.add(item);
+      }
+    });
+  }
+
+  void _handleItemOnLongPress(Item item){
+
+  }
 }
